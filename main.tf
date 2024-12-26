6fd53924efe5dd26740beec282349a35f442e67b
Joshua Geissler <jgeissler14@gmail.com>
2024-12-26 13:13:18 -0800

    chart

--- main.tf
+++ main.tf
@@ -26,7 +26,7 @@ provider "kubernetes" {
   client_certificate     = base64decode(data.azurerm_kubernetes_cluster.default.kube_config.0.client_certificate)
   client_key             = base64decode(data.azurerm_kubernetes_cluster.default.kube_config.0.client_key)
   cluster_ca_certificate = base64decode(data.azurerm_kubernetes_cluster.default.kube_config.0.cluster_ca_certificate)
-  config_path = "./kubeconfig"
+  config_path            = "./kubeconfig"
 }
 
 provider "helm" {
@@ -35,7 +35,7 @@ provider "helm" {
     client_certificate     = base64decode(data.azurerm_kubernetes_cluster.default.kube_config.0.client_certificate)
     client_key             = base64decode(data.azurerm_kubernetes_cluster.default.kube_config.0.client_key)
     cluster_ca_certificate = base64decode(data.azurerm_kubernetes_cluster.default.kube_config.0.cluster_ca_certificate)
-    config_path = "./kubeconfig"
+    config_path            = "./kubeconfig"
   }
 }
 
